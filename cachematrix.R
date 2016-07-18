16bd9d991df8dbd4773751656ba75e11558fdc9b
Kamili Talley <kamili@gmail.com>
2016-07-17 19:32:51 -0700

    R Programming Assignment 2 - Lexical Scoping
    Wrote 2 functions that calculate the inverse of a matrix and cache it to
    improve computation performance.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The makeCacheMatrix function creates an invertible matrix
+makeCacheMatrix <- function( m = matrix() ) {
+  theInverse = NULL # set inverse variable to null
+  setTheMatrix = function( a ) { # set the matrix
+    # assign a value to this object in a different environment than current one.
+    m <<- a 
+    theInverse <<- NULL 
+  }
+  getTheMatrix = function() m # get the matrix
+  setTheInverse = function( inverse ) theInverse <<- inverse # set the inverse
+  getTheInverse = function() theInverse # get the inverse
+  # list below goes into the cacheSolve() function
+  list( setTheMatrix = setTheMatrix, getTheMatrix = getTheMatrix, setTheInverse = setTheInverse, getTheInverse = getTheInverse )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# The cacheSolve function takes input from the makeCacheMatrix function and gets the inverse from cache.
+cacheSolve <- function( m, ... ) { ## Return the inverse matrix of 'm'
+  theInverse = m$getTheInverse()
+  if( !is.null( theInverse ) ) { ## If m's inverse already calculated...
+      message("get cached list") ## Get inverse from cache and don't compute
+      return ( theInverse )
+  }
+  ## else, calculate m's inverse
+  mat.data = m$getTheMatrix() 
+  theInverse = solve( mat.data, ... ) 
+  m$setTheInverse( theInverse ) ## set value of inverse in cache
+  return( theInverse ) 
 }
